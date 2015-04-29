ca474b419ce7fd3180bcae75032030ae28be93ba
Josh Bonador <Josh@Joshs-MacBook-Air.local>
2015-04-30 09:19:26 +1000

    Updated wordguess folder

--- josh_bonador/week_01/wordguess/main.js
+++ josh_bonador/week_01/wordguess/main.js
@@ -10,29 +10,31 @@
 // It should also figure out if there are any more letters that need to be guessed, and if not, it should congratulate the user for winning the game.
 // Pretend you don't know the word, and call guessLetter multiple times with various letters to check that your program works.
 
-// var currentWord = 'dodge'.split('');
-// var correctLetter = [];
-// correctLetter.length = currentWord.length;
-// var lettersLeft = correctLetter.length
+                                           // UNCOMMENT TO RUN WORD GUESSER FUNCTION
 
-// console.log('Input "guessLetter(letterOfYourChoice)" or "fortune(letter)" to start');
+var currentWord = 'dodge'.split('');
+var correctLetter = [];
+correctLetter.length = currentWord.length;
+var lettersLeft = correctLetter.length
 
-// var guessLetter = function(letter) {
+console.log('Input "guessLetter(letterOfYourChoice)" to start');
 
-// 	lettersLeft.length = correctLetter.length
-// 	for (var b = 0; b < currentWord.length; b++) {
-// 		if (letter === currentWord[b]) {                    
-// 			correctLetter[b] = (letter);                //passed letter is given correct spot in 2nd array      
-// 			console.log('You found a new letter!') 
-// 			console.log(correctLetter);
-// 			lettersLeft = lettersLeft - 1               //therefore the lettersLeft goes down by 1
-// 		}
-// 	}
-// 	if (lettersLeft === 0) {
-// 		console.log('You did it! The word is ' + currentWord.join(''))
-// 	} else if (lettersLeft > 0)
-// 	console.log('You have ' + lettersLeft + ' letters left.')
-// }
+var guessLetter = function(letter) {
+
+	lettersLeft.length = correctLetter.length
+	for (var b = 0; b < currentWord.length; b++) {
+		if (letter === currentWord[b]) {                    
+			correctLetter[b] = (letter);                //passed letter is given correct spot in 2nd array      
+			console.log('You found a new letter!') 
+			console.log(correctLetter);
+			lettersLeft = lettersLeft - 1               //therefore the lettersLeft goes down by 1
+		}
+	}
+	if (lettersLeft === 0) {
+		console.log('You did it! The word is ' + currentWord.join(''))
+	} else if (lettersLeft > 0)
+	console.log('You have ' + lettersLeft + ' letters left.')
+}
 
 // Bonus: Make it more like Wheel of Fortune:
 
@@ -40,35 +42,37 @@
 // Every time a letter is guessed, generate a random amount and reward the user if they found a letter (multiplying the reward if multiple letters found), otherwise subtract from their reward.
 // When they guess the word, log their final reward amount.
 
-var currentWord = 'dodge'.split('');
-var correctLetter = [];
-correctLetter.length = currentWord.length;
-var lettersLeft = correctLetter.length
-var reward = 0
-var subtractBy; 
+														//UNFINISHED
+
+// console.log('Input "fortune(letterOfYourChoice)" to start')
+
+// var currentWord = 'dodge'.split('');
+// var correctLetter = [];
+// correctLetter.length = currentWord.length;
+// var lettersLeft = correctLetter.length
+// var reward = 0
+// var subtractBy; 
 
 
-var fortune = function(letter) {
+// var fortune = function(letter) {
 
-	lettersLeft.length = correctLetter.length
-	for (var b = 0; b < currentWord.length; b++) {
-		if (letter === currentWord[b]) {                    
-			correctLetter[b] = (letter);                     
-			console.log('You found a new letter!')
-			reward += Math.round(Math.random())                    //reward is given a random amount
-			console.log(correctLetter);
-			lettersLeft = lettersLeft - 1
-		} else {
-		subtractBy = Math.round(Math.random())                     //reward is going to be subtracted by random amount
-		}
-	}
-	console.log('Your current reward is ' + (reward - subtractBy))   //reward is subtracted by random amount
-	if (lettersLeft === 0) {
-	console.log('You did it! The word is ' + currentWord.join(''))
-	} else if (lettersLeft > 0)
-	console.log('You have ' + lettersLeft + ' letters left.')
+// 	lettersLeft.length = correctLetter.length
+// 	for (var b = 0; b < currentWord.length; b++) {
+// 		if (letter === currentWord[b]) {                    
+// 			correctLetter[b] = (letter);                     
+// 			console.log('You found a new letter!')
+// 			reward += Math.round(Math.random())                    //reward is given a random amount
+// 			console.log(correctLetter);
+// 			lettersLeft = lettersLeft - 1
+// 		} 
+// 	}
+// 	console.log('Your current reward is ' + (reward - subtractBy))   //reward is subtracted by random amount
+// 	if (lettersLeft === 0) {
+// 	console.log('You did it! The word is ' + currentWord.join(''))
+// 	} else if (lettersLeft > 0)
+// 	console.log('You have ' + lettersLeft + ' letters left.')
 	
-}
+// }
 
 
 
