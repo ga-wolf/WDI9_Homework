46ac74c0954edacc8b7e4dab1665f2059abe6696
anonymous-wolf <jackjeffress@gmail.com>
2015-04-30 12:08:27 +1000

    Added warmup exercises

--- warmup_exercises/week_01/leap_year.js
+++ warmup_exercises/week_01/leap_year.js
@@ -22,3 +22,24 @@
 // - Ask the user what number they want to test
 // - Add the is Leap Year function to the number prototype...  Don't be too concerned about this.
 // - Watch a whole heap of information about leap years... http://www.youtube.com/watch?v=xX96xng7sAE
+
+var isLeapYear = function ( year ) {
+  // To go further
+  // year divided by 4 has to have no remainder - year % 4 
+  // year divided by 100 can't equal 0 - year % 100
+  // unless its divisible by 400 - year % 400
+  if ( year % 4 === 0 && ( !(year % 100 === 0) || year % 400 === 0 ) ) {
+    console.log( "Yep, " + year + " is a leap year." )
+  } else {
+    console.log( "Nope, " + year + " is not a leap year." )
+  }
+}
+
+isLeapYear( 1900 );
+isLeapYear( 1996 );
+isLeapYear( 1997 );
+isLeapYear( 2000 );
+
+// PROMPT SECTION
+
+isLeapYear( parseInt( prompt( "What year would you like to test?" ) ) );
