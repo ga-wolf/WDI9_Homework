b8f07eb449bf52d3ede4554fe4811dc54a1d08f5
Toby Flemming <tobyf93@hotmail.com>
2015-04-30 16:07:01 +1000

    Add bank account program

--- toby_flemming/week_01/2015-04-30/main.js
+++ toby_flemming/week_01/2015-04-30/main.js
@@ -75,13 +75,135 @@ var area = function(triangle) {
 }
 
 var isObtuse = function(triangle) {
-	//a^2 + b^2 < c^2
 	if (Math.pow(triangle.sideA, 2) + Math.pow(triangle.sideB, 2) < Math.pow(triangle.sideC, 2)) {
 		return true;
 	}
 
 	return false;
 }
+
+
+/*
+Bank
+
+There is only one bank. This bank has an array of accounts. The bank needs a method that will return the total sum of money in the accounts. It also needs an addAccount method that will enroll a new account at the bank and add it to the array of accounts. There is no need to create additional functions of the bank to delete accounts, etc.
+
+The bank has many accounts. Accounts should be objects that all share a set of common functionality.
+*/
+var bank = {
+	accounts: [],
+
+	sumMoney: function() {
+		var total = 0;
+		bank.accounts.forEach(function(account) {
+			total += account.balance;
+		});
+
+		console.log('Total sum of all accounts: $' + total);
+		return total;
+	},
+
+	addAccount: function(name) {
+		var newAccount;
+		if (name === undefined) {
+			console.log('Must provide account name.');
+		} else {
+			if (bank.accounts.length && bank.accounts.indexOf(name) >= 0) {
+				console.log('Account already exists');
+				return newAccount;
+			}
+			newAccount = new account(name);
+			bank.accounts.push(newAccount);
+			console.log('Account for ' + name + ' has been added.');
+		}
+
+		return newAccount;
+	},
+
+	findAccount: function(name) {
+		var matchedAccount;
+
+		for (var i = 0; i < bank.accounts.length; i++) {
+			var account = bank.accounts[i];
+
+			if (account.name === name) {
+				matchedAccount = account;
+				break;
+			}
+		}
+
+		return matchedAccount;
+	},
+
+	transfer: function(source, dest, amount) {
+		var sourceAcc = bank.findAccount(source);
+		var destAcc = bank.findAccount(dest);
+		var accountFound = true;
+
+		if (sourceAcc === undefined) {
+			console.log('No account exists for ' + source);
+			accountFound = false;
+		}
+		if (destAcc === undefined) {
+			console.log('No account exists for ' + dest);
+			accountFound = false;
+		}
+
+		if (!accountFound) {
+			return false;
+		}
+		if (!sourceAcc.withdraw(amount)) {
+			return false;
+		} else {
+			destAcc.deposit(amount);
+		}
+
+		console.log('$' + amount + ' has been transfered from ' + source + ' to ' + dest);
+	}
+};
+
+
+/*
+Accounts
+
+Accounts have a current balance and owner's name. You should be able to deposit or withdraw from an account to change the balance.
+
+There is no need to write a user interface. Make sure functions return values -- you may also have your functions console.log() values to help you see your code working.
+
+You should write a basic story through a series of JavaScript commands that shows that the methods do indeed work as expected: add some accounts, show the total balance, make some deposits and withdrawals, show the new total balance.
+*/
+function account(name) {
+	this.balance = 0;
+	this.name = name;
+
+	this.deposit = function(amount) {
+		this.balance += amount;
+
+		console.log('$' + amount + ' deposited successfully.  Balance is $' + this.balance);
+		return this.balance;
+	},
+
+	this.withdraw = function(amount) {
+		if (amount > this.balance) {
+			console.log('Insufficient funds.')
+			return false;
+		}
+
+		this.balance -= amount;
+		console.log('$' + amount + ' withdrawn successfully.  Balance is $' + this.balance);
+		return this.balance;
+	}
+};
+
+
+
+
+
+
+
+
+
+
 
 
 
