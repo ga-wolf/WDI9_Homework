97609b71ae7c9119864bcb6dd6e1c2ca6bb8824d
Mark Harris <harrisdm@hotmail.com>
2015-05-01 17:15:15 +1000

    mta working for trips on multiple lines

--- mark_harris/week_01/mta-1-js/main.js
+++ mark_harris/week_01/mta-1-js/main.js
@@ -20,10 +20,10 @@ var mta = [line_N, line_L, line_6];
 
 
 // Given as starting and stoping index, return an array of stop names between them
-var createStopList = function(start, end) {
+var createStopList = function(line, start, end) {
 	stopList = [];
 	while(start <= end) {
-		stopList.push(line_N.stations[start])
+		stopList.push(mta[line].stations[start])
 		start++;
 	}
 	return stopList;
@@ -37,13 +37,23 @@ var planTrip = function(startLineName, startStation, endLineName, endStation) {
 	// Find the stations on the starting line
 	for(var i = 0; i < mta.length; i++) {
 		if(mta[i].name === startLineName) {
-			startLine = i;
+			startLineIndex = i;
 		}
 	}
-	var allLineStops =  mta[startLine].stations;
+	var allLineStops =  mta[startLineIndex].stations;
+	console.log(allLineStops);
+
+	// Find the stations on the ending line
+	for(var i = 0; i < mta.length; i++) {
+		if(mta[i].name === endLineName) {
+			endLineIndex = i;
+		}
+	}
+	var allLineStops =  mta[endLineIndex].stations;
 	console.log(allLineStops);
 
 
+	// Check if the user is already at the destination
 	if(startLineName === endLineName && startStation === endStation) {
 		alert("You are already at your destination");
 	} else {
@@ -51,20 +61,21 @@ var planTrip = function(startLineName, startStation, endLineName, endStation) {
 
 		// Check if the stops are on the same line
 		if(startLineName === endLineName) {
+
 			// Find the index of the starting station
-			startStationIndex = mta[startLine].stations.indexOf(startStation);
+			startStationIndex = mta[startLineIndex].stations.indexOf(startStation);
 			console.log(startStationIndex);
 
 			// Find the index of the end station
-			endStationIndex = mta[startLine].stations.indexOf(endStation);
+			endStationIndex = mta[startLineIndex].stations.indexOf(endStation);
 			console.log(endStationIndex);
 
 			// Create a list of stations that are passed through
 			var lineStations = [];
 			if(startStationIndex <= endStationIndex) {
-				lineStations = createStopList(startStationIndex, endStationIndex).slice(1);
+				lineStations = createStopList(startLineIndex, startStationIndex, endStationIndex).slice(1);
 			} else {
-				lineStations = createStopList(endStationIndex, startStationIndex).reverse().slice(1);
+				lineStations = createStopList(startLineIndex, endStationIndex, startStationIndex).reverse().slice(1);
 			}
 			
 			console.log("You must travel through the following stops on the " + startLineName + " Line: " 
@@ -72,7 +83,50 @@ var planTrip = function(startLineName, startStation, endLineName, endStation) {
 			console.log(lineStations.length + " stops in total.");
 		}
 		else {
+			// Find the index of the starting station
+			startStationIndex = mta[startLineIndex].stations.indexOf(startStation);
+			console.log(startStationIndex);
 
+			// Find the index of the end station
+			endStationIndex = mta[endLineIndex].stations.indexOf(endStation);
+			console.log(endStationIndex);
+
+			var toUS = mta[startLineIndex].stations.indexOf("Union Square");
+			var fromUS = mta[endLineIndex].stations.indexOf("Union Square");
+			console.log(toUS);
+			console.log(fromUS);
+
+			numTripStops = 0;
+
+			// Create a list of stations that are passed through going to Union Square
+			var lineStations = [];
+			if(startStationIndex <= toUS) {
+				lineStations = createStopList(startLineIndex, startStationIndex, toUS).slice(1);
+			} else {
+				lineStations = createStopList(startLineIndex, toUS, startStationIndex).reverse().slice(1);
+			}
+
+
+
+			console.log("You must travel through the following stops on the " + startLineName + " Line: " 
+				+ lineStations.join(", "));
+			console.log("Change at Union Square.");
+			numTripStops += lineStations.length;
+
+
+			// Create a list of stations that are passed through going from Union Square
+			var lineStations = [];
+			if(endStationIndex <= fromUS) {
+				lineStations = createStopList(endLineIndex, endStationIndex, fromUS).reverse().slice(1);
+			} else {
+				lineStations = createStopList(endLineIndex, fromUS, endStationIndex).slice(1);
+			}
+			console.log(lineStations);
+			numTripStops += lineStations.length;
+
+
+			console.log("Your journey continues through the following stops: " + lineStations.join(", "));
+			console.log(numTripStops + " stops in total.");
 		}
 	}
 
