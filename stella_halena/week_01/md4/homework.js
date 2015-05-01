9a97a44b19c721d23b15230eb49daa6669fd6b92
stella <stella@stellas-MacBook-Pro.local>
2015-05-01 16:14:55 +1000

    module 4 homework bank

---
+++ stella_halena/week_01/md4/homework.js
@@ -0,0 +1,110 @@
+var rectangle = {
+	length : 16,
+	width: 8,
+};
+
+var l = rectangle.length
+var w = rectangle.width
+
+var rectangleArea = w * l;
+var rectanglePerim = (w + l) + (w + l);
+
+
+if ( w === l ) { 
+
+	console.log (" the area of the shape is " + rectangleArea 
+		+ " while the perimeter is " + rectanglePerim + " and this is a square ");
+	
+	} else {
+
+	console.log(" the area of the shape is " + rectangleArea 
+		+ " while the perimeter is " + rectanglePerim + " and this is not square ")
+}
+
+
+
+
+
+
+
+var rectangle = {
+	length : 16,
+	width: 16,
+};
+
+var l = rectangle.length
+var w = rectangle.width
+
+var rectangleArea = w * l;
+var rectanglePerim = (w + l) + (w + l);
+
+
+if ( w === l ) { 
+
+	console.log (" the area of the shape is " + rectangleArea 
+		+ " while the perimeter is " + rectanglePerim + " and this is a square ");
+	
+	} else {
+
+	console.log(" the area of the shape is " + rectangleArea 
+		+ " while the perimeter is " + rectanglePerim + " and this is not square ")
+}
+
+
+
+
+var triangle = {
+	sideA: 16,
+	sideB: 16,
+	sideC: 16,
+	sideH: 9, 
+};
+
+var A = triangle.sideA
+var B = triangle.sideB
+var C = triangle.sideC
+var H = triangle.sideH
+
+
+var triangleArea = (H * B) / 2;
+var trianglePerim = A + B + C;
+
+console.log("the sides of the trinagle are : " + 
+							triangle.sideA + triangle.sideB 
+							+ triangle.sideC ); 
+console.log( )
+
+
+	if ( A * B * triangleArea > 90 ) { 
+
+	console.log (" this is an obtuse triangle ");
+	
+	} else if ( A === B && B === C ) {
+	console.log(" this is an equilateral triangle "); 
+	
+	} else if ( A === B ) { 
+
+		console.log ("this is an isoceles triangle")
+	}
+
+
+var triangle = {
+	sideA: 16,
+	sideB: 16,
+	sideC: 16,
+	sideH: 9, 
+};
+
+var isObtuse = function (triangle) {
+
+}
+
+triangle 
+
+
+
+
+
+
+
+
