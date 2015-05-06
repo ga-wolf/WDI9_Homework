94c7324ab47a7de2404c84f6e148382767381c36
Mark Harris <harrisdm@hotmail.com>
2015-05-06 18:59:39 +1000

    wk2-hwk2 Cat loops across the screen

--- mark_harris/week_02/html_css-homework-3/js/main.js
+++ mark_harris/week_02/html_css-homework-3/js/main.js
@@ -2,12 +2,12 @@
 var cat = document.getElementById("catPic");
 cat.style.top = '0px';
 cat.style.left = '0px';
-cat.style.width = "296px";
-cat.style.height = "296px";
-
+//cat.style.width = "296px";
+//cat.style.height = "296px";
+/*
 var catWalk = function () {
   var newTop = parseInt(cat.style.top) + 1;
-  cat.style.top = newTop + 'px';
+  //cat.style.top = newTop + 'px';
 
   var newLeft = parseInt(cat.style.left) + 5;
   cat.style.left = newLeft + 'px';
@@ -17,11 +17,36 @@ var catWalk = function () {
 
   var newHeight = parseInt(cat.style.height) + 1;
   //cat.style.height = newHeight + 'px';
+}
+*/
 
+var catWalkForever = function () {
+  var currentLeft = parseInt(cat.style.left);
+  var newLeft = currentLeft + 5;
+  cat.style.left = newLeft + 'px';
 
+  if(currentLeft > window.innerWidth) {
+    cat.style.left = "0px";
+  }
 }
 
-window.setInterval(catWalk, 40);
+/*
+  var width = window.innerWidth;
+  //console.log(width);
+  //var catLeft = parseInt(cat.style.left.slice(0,-2));
+
+var x = 100;
+  if(x < 300) {
+    //alert("gone");
+    console.log(x);
+    x = x + 50;
+  }
+*/
+
+
+//window.setInterval(catWalk, 40);
+window.setInterval(catWalkForever, 40);
+//console.log(window.innerWidth);
 
 
 
