954adc49f60990e184ac25e00f2e21769f42b0d7
Toby Flemming <tobyf93@hotmail.com>
2015-05-07 22:22:03 +1000

    Bind values to this/that variables.  Deposit/withdraw working.

--- toby_flemming/week_02/2015-05-07/ga-bank/js/atm.js
+++ toby_flemming/week_02/2015-05-07/ga-bank/js/atm.js
@@ -1,36 +1,61 @@
-var atm = atm || {};
-
-
-
-
-
-
-
-var Account = function(name) {
+var Account = function(type) {
 	this.balance = 0;
+	this.type = type;
+	this.prefix = '#' + type;	// So i don't have to string concat (much)
 };
 
-Account.prototype.deposit = function() {
+Account.prototype.deposit = function(that) {
 	var $account = $(this.parentElement);
-	var type = $account.attr('id');
-	var idPrefix = '#' + type;
 
-	amount = $(idPrefix + '-amount', $account).val();
+	var amount = $(that.prefix + '-amount', $account).val();
 	amount = parseInt(amount) || 0;
 
-	var $balance = $(idPrefix + '-balance', $account);
-	this.balance += amount;
-	console.log(this);
-	$balance.html('$' + this.balance);
+	var $balance = $(that.prefix + '-balance', $account);
+	that.balance += amount;
+	$balance.html('$' + that.balance);
 };
 
-Account.prototype.withdraw = function(amount) {
-	if (this.amount > amount) {
-		this.amount -= amount;
+Account.prototype.withdraw = function(that) {
+	var $account = $(this.parentElement);
+	var idPrefix = '#' + that.type;
+
+	var amount = $(that.prefix + '-amount', $account).val();
+	amount = parseInt(amount) || 0;
+
+	var $balance = $(that.prefix + '-balance', $account);
+
+	if (that.balance >= amount) {
+		that.balance -= amount;
+		$balance.html('$' + that.balance);
 	}
 };
 
-atm.checking = new Account();
-atm.savings = new Account();
 
-$('#checking-deposit').on('click', atm.checking.deposit.bind(atm.checking));
+
+
+
+
+var atm = atm || {};
+
+atm.createAccounts = function() {
+	atm.checking = new Account('checking');
+	atm.savings = new Account('savings');
+};
+
+// Setting up event listeners
+// Bind was used to ensure the function called had reference to this and that after event has been
+// fired.  Need to find out how much of a hack this is...
+atm.setupEvents = function() {
+	$('#checking-deposit').on('click', function() { atm.checking.deposit.bind(this, atm.checking)(); });
+	$('#checking-withdraw').on('click', function() { atm.checking.withdraw.bind(this, atm.checking)(); });
+
+	$('#savings-deposit').on('click', function() { atm.savings.deposit.bind(this, atm.savings)(); });
+	$('#savings-withdraw').on('click', function() { atm.savings.withdraw.bind(this, atm.savings)(); });
+};
+
+atm.main = function() {
+	this.createAccounts();
+	this.setupEvents();
+};
+
+atm.main();
