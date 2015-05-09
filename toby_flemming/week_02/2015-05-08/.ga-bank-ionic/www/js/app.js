80fa59da7f370fe157e44445a691a1173c2759ee
Toby Flemming <tobyf93@hotmail.com>
2015-05-10 08:07:10 +1000

    Create directory for new work.

--- toby_flemming/week_02/2015-05-08/.ga-bank-ionic/www/js/app.js
+++ toby_flemming/week_02/2015-05-08/.ga-bank-ionic/www/js/app.js
@@ -5,6 +5,18 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('atm', ['ionic'])
 
+.config(['$ionicAppProvider', function($ionicAppProvider) {
+  // Identify app
+  $ionicAppProvider.identify({
+    // The App ID (from apps.ionic.io) for the server
+    app_id: '024a4fb8',
+    // The public API key all services will use for this app
+    api_key: '07e8c909f12d10142413b0a7f1bf5a1d57dfdeeee881f2cd',
+    // The GCM project ID (project number) from your Google Developer Console (un-comment if used)
+    // gcm_id: 'YOUR_GCM_ID'
+  });
+}])
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -16,4 +28,4 @@ angular.module('atm', ['ionic'])
 
     atm.main();
   });
-});
+})
