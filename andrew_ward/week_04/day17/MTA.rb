4dfbced2305e32166803e5bacd6e8edc330b27a1
randbw <apbward@gmail.com>
2015-05-19 19:17:06 +1000

    MTA RUBY mostly working SWEEEEET

---
+++ andrew_ward/week_04/day17/MTA.rb
@@ -0,0 +1,161 @@
+# Subway
+
+# Union Square Index in different lines hardcoded like a nub
+# union_line_N = lines[:N].find_index "Union Square"
+# union_line_L = lines[:L].find_index "Union Square"
+# union_line_Six = lines[:Six].find_index "Union Square"
+
+def starting_line
+  puts "What line are you starting your journey on? N, L or Six?"
+  gets.chomp.capitalize.to_sym
+end
+
+def index_of_start_stop (start_line, lines)
+  puts "What station are you beginning your journey at?"
+  starting_stop = gets.chomp
+  puts "You will start your journey at #{starting_stop} on line #{start_line.to_s}"
+  lines[start_line].find_index(starting_stop)
+end
+
+def start_line_union_square_index (start_line, lines)
+  lines[start_line].index "Union Square"
+end
+
+def first_trip_function (array)
+  trip = "Your trip will take you past "
+  array.each do |stop|
+    trip += "#{stop} and "
+  end
+  puts trip + "."
+end
+
+def destination_line_union_square_index (dest_line, lines)
+  lines[dest_line].index "Union Square"
+end
+
+def destination_line
+  puts "What line are you ending your journey on? N, L or Six?"
+  gets.chomp.capitalize.to_sym
+end
+
+def index_of_final_stop (dest_line,lines)
+  puts "What station are you ending your journey at?"
+  final_stop = gets.chomp
+  puts "You will end your journey at #{final_stop} on line #{dest_line.to_s}"
+  lines[dest_line].index(final_stop)
+end
+
+def final_trip_function (array)
+  trip = "Your trip will take you past "
+  array.each do |stop|
+    trip += " #{stop} and "
+  end
+  puts trip + "."
+end
+
+
+
+#################################################################
+
+#                     THE BIG DOG                               #
+
+#################################################################
+
+
+def the_big_function
+
+  lines = {
+    :N => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
+    :L => ["8th", "6th", "Union Square", "3rd", "1st"],
+    :Six => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
+  }
+
+  # Find start line
+  start_line = starting_line
+
+  # Index of starting station
+  start_ind = index_of_start_stop(start_line, lines)
+
+  # Union Square index of first line
+  start_union_index = start_line_union_square_index(start_line,lines)
+
+  # Determines which direction and how far first trip is
+  first_trip_length = start_union_index - start_ind
+
+  # Array of first trip
+  if first_trip_length < 0
+    first_trip = lines[start_line][start_union_index..start_ind].reverse
+  elsif first_trip_length > 0
+    first_trip = lines[start_line][start_ind..start_union_index]
+  elsif first_trip_length == 0
+    puts "CHEERS M8"
+  end
+
+  # First trip function
+  first_trip_function first_trip
+  puts "Your trip on line #{start_line} is #{first_trip.length} stops long."
+
+  # Find final line
+  final_line = destination_line
+
+  # Union Square index of second line
+  dest_union_index = destination_line_union_square_index(final_line, lines)
+
+  # Index of destination
+  final_index = index_of_final_stop(final_line,lines)
+
+  # Determines which direction and how far final trip is
+  final_trip_length = final_index - dest_union_index
+
+  # Array of second trip
+  if final_trip_length < 0
+    final_trip = lines[final_line][final_index..dest_union_index].reverse
+  elsif final_trip_length > 0
+    final_trip = lines[final_line][dest_union_index..final_index]
+  elsif final_trip_length == 0
+    puts "CHEERS M8"
+  end
+
+  # Final trip function
+  final_trip_function final_trip
+  puts "Your trip on line #{final_line} is #{final_trip_length} stops long."
+
+
+
+  # LENGTH OF TRIP IS first_trip.length + final_trip.length
+
+end
+
+the_big_function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- andrew_ward/week_04/warmUps/pling_plang_plong/pling_plang_plong.rb
+++ andrew_ward/week_04/warmUps/pling_plang_plong/pling_plang_plong.rb
@@ -5,9 +5,9 @@
 
 # If the number does not contain 3, 5, or 7 as a prime factor, simply return the string representation of the number itself.
 
+## My solution
 
-
-def pling_plang_plong (num)
+def pling_plang_plong num
 
   if num % 3 == 0
     print "Pling "
@@ -26,8 +26,38 @@ def pling_plang_plong (num)
   end
 end
 
-
-puts "Please select a number"
+print "Please choose a number: "
 number = gets.to_i
 
 puts pling_plang_plong(number)
+
+## Jack's solution - uses implicit return. More readable
+
+def fetch_number
+  print "Please choose a number: "
+  gets.to_i
+end
+
+def divisible_by_three? number
+  number % 3 == 0
+end
+
+def disible_by_five? number
+  number % 5 == 0
+end
+
+def divisible_by_seven? number
+  number % 7 == 0
+end
+
+def convert_string
+  number = fetch_number
+  string = ""
+  string += "Pling " if divisible_by_three? number
+  string += "Plang " if disible_by_five? number
+  string += "Plong " if divisible_by_seven? number
+
+  string
+end
+
+puts convert_string
