3113c6f272d22f76677b989991147b62813f4052
Toby Flemming <tobyf93@hotmail.com>
2015-05-19 19:26:12 +1000

    Start of an object-orientated approach

--- toby_flemming/week_04/2015-05-19-mta/mta.rb
+++ toby_flemming/week_04/2015-05-19-mta/mta.rb
@@ -1,90 +1,93 @@
-def show_menu
-  puts 'BASIC FUNCTIONALITY:'
-  puts '[+]  Addition'
-  puts '[-]  Subtraction'
-  puts '[*]  Multiplication'
-  puts '[/]  Division'
-  puts ''
-  puts 'ADVANCED FUNCTIONALITY:'
-  puts '[^]  Exponent'
-  puts '[s]  Square Root'
-  puts ''
-  puts '[q]  Quit Program'
-end
+class Line
 
-def get_string(msg)
-  print msg
-  gets.chomp.downcase
-end
-
-def get_float(msg)
-  get_string(msg).to_f
-end 
-
-def addition
-  num1 = get_float 'Number 1: '
-  num2 = get_float 'Number 2: '
-
-  puts num1 + num2
-end
-
-def subtraction
-  num1 = get_float 'Number 1: '
-  num2 = get_float 'Number 2: '
-
-  puts num1 - num2
-end
-
-def multiplication
-  num1 = get_float 'Number 1: '
-  num2 = get_float 'Number 2: '
-
-  puts num1 * num2
-end
-
-def division
-  num1 = get_float 'Number 1: '
-  num2 = get_float 'Number 2: '
-
-  puts num1 / num2
-end
-
-def exponent
-  base = get_float 'Number: '
-  exponent = get_float 'Exponent: '
-
-  puts base ** exponent
-end
-
-def square_root
-  num = get_float 'Number: '
-
-  puts Math.sqrt num
-end
-
-def main
-  selection = '!'
-  show_menu
-
-  while selection != 'q'
-    selection = get_string 'Please Select An Option: '
-
-    case selection
-    when '+'
-      addition
-    when '-'
-      subtraction
-    when '*'
-      multiplication
-    when '/'
-      division
-    when '^'
-      exponent
-    when 's'
-      square_root
-    end
-    
+  def initialize name, stations
+    @name = name
+    @stations = stations
   end
+
+  def self.travel start_station, end_station
+  end
+
+  # Getters
+  def get_name
+    @name
+  end
+
+  def get_stations
+    @stations
+  end
+
 end
 
-main
+
+
+
+
+
+
+
+
+
+class MTA
+
+  # Initialize the program with pre-defined lines/stations
+  def initialize
+    @lines = []
+
+    @lines << Line.new("N", ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"])
+    @lines << Line.new("L", ["8th", "6th", "Union Square", "3rd", "1st"])
+    @lines << Line.new("6", ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"])
+  end
+
+  # Instance Methods
+  def plan_trip start_line, start_station, end_line, end_station
+    line1 = find_line start_line
+    line2 = find_line end_line
+
+    # Validate lines
+    if !line1 || !line2
+
+    if line1 == line2
+      line1.travel start_line, end_station
+    else
+      line1.travel start_line, "Union Square"
+      line2.travel "Union Square", end_line
+    end
+  end
+
+  def find_line name
+    result = nil
+
+    @lines.each do |line|
+      result = line if line.get_name == name
+    end
+
+    result
+  end
+
+end
+
+
+mta = MTA.new
+
+# puts mta.find_line "N"
+mta.plan_trip "N", "", "N", ""
+
+
+
+# line = new Line('N', ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th']);
+#   this.lines.push(line);
+#   line = new Line('L', ['8th', '6th', 'Union Square', '3rd', '1st']);
+#   this.lines.push(line);
+#   line = new Line('6', ['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place']);
+# var MTA = {};
+# MTA.lines = [];
+# MTA.findLine = function(name) {
+# MTA.planTrip = function(startLine, startStation, endLine, endStation) {
+# MTA.loadLines = function() {
+# MTA.main = function() {
+
+
+
+
+
