d0d2c8ef903468c8261d317d1ba033eca8c2ceaf
Toby Flemming <tobyf93@hotmail.com>
2015-05-20 23:08:22 +1000

    Create animals/clients into shelter hash

--- toby_flemming/week_04/2015-05-20-happitails/client.rb
+++ toby_flemming/week_04/2015-05-20-happitails/client.rb
@@ -1,15 +1,15 @@
 class Client
   attr_reader :name, :children, :age, :pets
 
-  def initialize name, children, age, pets
+  def initialize name, children, age
     @name = name
     @children = children
     @age = age
-    @pets = pets
+    @pets = []
   end
 
   def to_s
-    "#{@name.upcase} - #{@age}yo with #{children} children and #{pets} pets"
+    "#{@name.upcase} - #{@age}yo with #{children} children and #{pets.length} pets"
   end
 
 end
--- toby_flemming/week_04/2015-05-20-happitails/main.rb
+++ toby_flemming/week_04/2015-05-20-happitails/main.rb
@@ -12,12 +12,12 @@ class Happitails
     @shelter = {:animals => [], :clients => []}
   end
 
-  def give_up_animal
-    # Will need prompt
+  def get_string
+    gets.chomp.upcase
   end
 
-  def adopt_animal
-    # Will need prompt
+  def get_fixnum
+    get_string.to_i
   end
 
   def show_menu
@@ -48,11 +48,49 @@ class Happitails
   end
 
   def display_animals
-    @shelter[:animals].each { |animal| puts animal.to_s }
+    @shelter[:animals].each_with_index { |animal, i| puts "[#{i}] #{animal.to_s}" }
   end
 
   def display_clients
-    @shelter[:clients].each { |client| puts client.to_s }
+    @shelter[:clients].each_with_index { |client, i| puts "[#{i}] #{client.to_s}" }
+  end
+
+  def create_animal
+    print "Name: "
+    name = get_string
+
+    print "Age: "
+    age = get_fixnum
+
+    print "Gender: "
+    gender = get_string
+
+    print "Species: "
+    species = get_string
+
+    @shelter[:animals] << Animal.new(name, age, gender, species)
+  end
+
+  def create_client
+    print "Name: "
+    name = get_string
+
+    print "No of Children: "
+    children = get_fixnum
+
+    print "Age: "
+    age = get_fixnum
+
+    shelter[:clients] << Client.new(name, children, age)
+  end
+
+  def adopt_animal
+    print "Animal Name: "
+
+  end
+
+  def give_up_animal
+
   end
 
   def main
@@ -65,6 +103,9 @@ class Happitails
 
       display_animals if option == 'da'
       display_clients if option == 'dc'
+      create_animal if option == 'ca'
+      create_client if option == 'cc'
+
       show_menu if option == 'c'
     end
   end
@@ -88,8 +129,8 @@ happitails.shelter[:animals] << Animal.new("Sally", 1, "F", "Dog")
 happitails.shelter[:animals] << Animal.new("Goldy", 15, "M", "Fish")
 
 # Clients
-happitails.shelter[:clients] << Client.new("Toby", 0, 22, 0)
-happitails.shelter[:clients] << Client.new("Crazy Cat Lady", 0, 85, 100)
+happitails.shelter[:clients] << Client.new("Toby", 0, 22)
+happitails.shelter[:clients] << Client.new("Crazy Cat Lady", 0, 85)
 
 # binding.pry
 happitails.main
