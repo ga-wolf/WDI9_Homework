789b9d98790c630978d3c0ecf03cc48b1fb82421
randbw <apbward@gmail.com>
2015-05-21 17:28:55 +1000

    Got the basic movie search function complete

---
+++ andrew_ward/week_04/day19/movies/main.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+
+get '/' do
+
+  @title = params[:title]
+  url = "http://omdbapi.com/?t=#{@title}"
+  movie_data = HTTParty.get url
+  @plot = movie_data['Plot']
+  @poster = movie_data['Poster']
+  @title_proper = movie_data['Title']
+  @imdbRating = movie_data['imdbRating']
+  @released = movie_data['Released']
+
+erb :movie
+
+end
+
+
+##### TO GET MOVIES WITH SPACES IN IT YOU HAVE TO INSERT A PLUS WHEN THERE SHOULD BE A SPACE
+
+##### TO GET MULTIPLE TITLES SEARCH WITH 'S' RATHER THAN 'T' IN THE URL
