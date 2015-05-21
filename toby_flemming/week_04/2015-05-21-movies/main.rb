d722d37135158318e3bbc2d8cfb9955c00bda01e
Toby Flemming <tobyf93@hotmail.com>
2015-05-21 20:49:41 +1000

    Search working with single/multiple matches

---
+++ toby_flemming/week_04/2015-05-21-movies/main.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+
+get '/' do
+  @search_str = params[:search_str]
+  url = "http://omdbapi.com/?s=#{@search_str}"
+  @movies = HTTParty.get(url)["Search"]
+
+  # 's' option giving a compact result for each movie.  We want the full thing...
+  if @movies
+    @movies.map! do |movie|
+      id = movie["imdbID"]
+      url = "http://omdbapi.com/?i=#{id}"
+
+      complete_data = HTTParty.get(url)
+      movie.merge complete_data
+    end
+  end
+
+  erb :movies
+end
+
+
+
+get '/movie/:id' do
+
+end
