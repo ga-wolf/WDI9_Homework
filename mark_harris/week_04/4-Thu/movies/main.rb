646f15155b034ca2176a7a9431ab79e41a94ffa2
Mark Harris <harrisdm@hotmail.com>
2015-05-21 23:04:43 +1000

    Search handles '' and failed searches

--- mark_harris/week_04/4-Thu/movies/main.rb
+++ mark_harris/week_04/4-Thu/movies/main.rb
@@ -12,25 +12,39 @@ end
 
 
 
-
 get "/search" do
   @film = params[:title]
 
   url = "http://www.omdbapi.com/?s=#{ @film }"
   @search_data = HTTParty.get(url)
-  data = HTTParty.get(url)
 
-   if @search_data["Search"].length == 1
-     url = "http://www.omdbapi.com/?t=#{ @search_data["Search"][0]["Title"] }"
-     @film_data = HTTParty.get(url)
-     erb :film
-   else
-    erb :search
-   end
+
+  # if no results       ["Response"] = false
+  # if movie not found  ["Response"] = false
+  # if many results     ["Response"] doesn't exist but ["Search"] exists
+  # if one results      ["Response"] doesn't exist but ["Search"] exists
+
+  if @search_data.has_key?("Search")
+
+    if @search_data["Search"].length == 1
+   
+      url = "http://www.omdbapi.com/?t=#{ @search_data["Search"][0]["Title"] }"
+      @film_data = HTTParty.get(url)
+      erb :film
+ 
+    else
+ 
+      erb :search
+ 
+    end
+
+  else
+
+    erb :error
+  
+  end
 
 end
-
-
 
 
 
@@ -40,18 +54,6 @@ get "/film" do
   url = "http://www.omdbapi.com/?t=#{ @film }"
   @film_data = HTTParty.get(url)
 
-  # if film_data["Response"]
-  #   @title = film_data["Title"]
-  #   @year = film_data["Year"]
-  #   @rated = film_data["Rated"]
-  #   @released = film_data["Released"]
-  #   @runtime = film_data["Runtime"]
-  #   @genre = film_data["Genre"]
-  #   @director = film_data["Director"]
-  #   @actors = film_data["Actors"]
-  #   @plot = film_data["Plot"]
-  #   @poster = film_data["Poster"]
-  # end
   if @film_data["Response"]
     erb :film
   end
