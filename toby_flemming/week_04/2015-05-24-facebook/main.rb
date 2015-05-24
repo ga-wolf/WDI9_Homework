48e9636f26cc6c5266823e168e37179c7e7de726
Toby Flemming <tobyf93@hotmail.com>
2015-05-24 13:40:02 +1000

    Abandon app: facebook API only shows friends using app (fucking cunts)

--- toby_flemming/week_04/2015-05-24-facebook/main.rb
+++ toby_flemming/week_04/2015-05-24-facebook/main.rb
@@ -3,7 +3,14 @@ require 'sinatra/reloader'
 require 'httparty'
 
 
-get '/friends' do
+get '/' do
+  erb :home
+end
+
+get '/friends' do 
+  token = params["token"]
+
+  @result = HTTParty.get("https://graph.facebook.com/me?access_token=#{token}")
   erb :friends_index
 end
 
--- toby_flemming/week_04/2015-05-24-facebook/public/js/main.js
+++ toby_flemming/week_04/2015-05-24-facebook/public/js/main.js
@@ -3,11 +3,12 @@ var app = app || {};
 app.btnClick = function() {
   var status = app.checkStatus();
 
-  if (status === 'connected') {
-    FB.logout();
-  } else {
+  if (status !== 'connected') {
     FB.login();
   }
+
+  var qs = '?token=' + app.token;
+  window.location.href = '/friends' + qs;
 };
 
 app.checkStatus = function() {
