eb4bc1304c3e514482d755292ff56b4000b8611a
Toby Flemming <tobyf93@hotmail.com>
2015-05-25 20:03:37 +1000

    Cache searches.  Change deserialization to JSON

--- toby_flemming/week_04/2015-05-21-movies/main.rb
+++ toby_flemming/week_04/2015-05-21-movies/main.rb
@@ -12,6 +12,7 @@ ActiveRecord::Base.establish_connection(
 ActiveRecord::Base.logger = Logger.new(STDERR)
 
 require_relative 'movie'
+require_relative 'search'
 
 after do
   ActiveRecord::Base.connection.close
@@ -24,8 +25,19 @@ def db_add_movie data
   if movie.empty?
     movie = Movie.new
     movie.imdb_id = imdb_id
-    movie.data = data.to_yaml
+    movie.data = data.to_json
     movie.save
+  end
+end
+
+def db_add_search search_str, data
+  prev_searches = Search.where(:search_str => search_str)
+
+  if prev_searches.empty?
+    search = Search.new
+    search.search_str = search_str
+    search.data = data.to_json
+    search.save
   end
 end
 
@@ -35,23 +47,28 @@ end
 
 # New movie search - store results in db
 get '/movies' do
-  @search_str = params[:search_str]
-  url = "http://omdbapi.com/?s=#{@search_str}"
+  search_str = params[:search_str].strip.downcase
+  url = "http://omdbapi.com/?s=#{search_str}"
 
-  @movies = HTTParty.get(url)["Search"] unless !@search_str
+  # Have we previously searched for this?
+  prev_search = Search.where(:search_str => search_str).first[:data]
+  @movies = JSON.parse prev_search
+
+  @movies = HTTParty.get(url)["Search"]
+  redirect to '/' if search_str.empty? || !@movies
 
   # 's' option giving a compact result for each movie.  We want the full thing...
-  if @movies
-    @movies.map! do |movie|
-      id = movie["imdbID"]
-      url = "http://omdbapi.com/?i=#{id}"
+  @movies.map! do |movie|
+    id = movie["imdbID"]
+    url = "http://omdbapi.com/?i=#{id}"
 
-      complete_data = HTTParty.get(url)
-      db_add_movie complete_data
+    complete_data = HTTParty.get(url)
+    db_add_movie complete_data
 
-      complete_data
-    end
+    complete_data
   end
+
+  db_add_search search_str, @movies
 
   if @movies.length == 1
     movie = @movies.first
@@ -64,7 +81,7 @@ end
 
 get '/movies/:id' do
   movie_record = Movie.where(:imdb_id => params[:id]).first
-  @movie = YAML.load movie_record[:data]
+  @movie = JSON.parse movie_record[:data]
 
   erb :movies_show
 end
